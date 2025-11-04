e41c3c71c6b77ffc35dd03e2b88ca3d2fe503206
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-11-04 17:18:35 -0400

    Update ProductRoutes.tsx

--- src/routes/ProductRoutes.tsx
+++ src/routes/ProductRoutes.tsx
@@ -1,52 +1,28 @@
-// routes/ProductRoutes.tsx
+// routes/ProductRoutes.tsx (Simplified Version)
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Navigate } from "react-router-dom";
 import CachedRoute from "../components/CachedRoute";
 import ProductDetail from "../pages/ProductDetail";
 import SingleProductDetail from "../pages/SingleProductDetail";
 import ProductDescriptionPage from "../pages/ProductDescriptionPage";
 import ProductCommentsPage from "../pages/ProductCommentsPage";
-import ProductReviewsPage from "../pages/ProductReviewsPage";
 import ProductQAPage from "../pages/ProductQAPage";
 import AskQuestionPage from "../pages/AskQuestionPage";
 
 export function ProductRoutes() {
   return (
     <>
-      {/* Main product detail routes with tab support */}
+      {/* Single catch-all route for all product tabs */}
+      <Route path="product/:id/*" element={
+        <CachedRoute>
+          <ProductDetail />
+        </CachedRoute>
+      } />
+
+      {/* Redirect base product route to overview */}
       <Route path="product/:id" element={
         <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/overview" element={
-        <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/variants" element={
-        <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/reviews" element={
-        <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/store-reviews" element={
-        <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/reviews-gallery" element={
-        <CachedRoute>
-          <ProductDetail />
-        </CachedRoute>
-      } />
-      <Route path="product/:id/qna" element={
-        <CachedRoute>
-          <ProductDetail />
+          <Navigate to="overview" replace />
         </CachedRoute>
       } />
 
@@ -56,16 +32,19 @@ export function ProductRoutes() {
           <ProductDescriptionPage />
         </CachedRoute>
       } />
+      
       <Route path="product/:id/comments" element={
         <CachedRoute>
           <ProductCommentsPage />
         </CachedRoute>
       } />
+      
       <Route path="product/:id/qa" element={
         <CachedRoute>
           <ProductQAPage />
         </CachedRoute>
       } />
+      
       <Route path="product/:id/ask-question" element={
         <CachedRoute>
           <AskQuestionPage />
@@ -78,21 +57,16 @@ export function ProductRoutes() {
           <SingleProductDetail />
         </CachedRoute>
       } />
+      
       <Route path="single-product/:id/comments" element={
         <CachedRoute>
           <ProductCommentsPage />
         </CachedRoute>
       } />
+      
       <Route path="single-product/:id/ask-question" element={
         <CachedRoute>
           <AskQuestionPage />
-        </CachedRoute>
-      } />
-
-      {/* Redirects for old routes to new tab system */}
-      <Route path="product/:id/" element={
-        <CachedRoute>
-          <ProductDetail />
         </CachedRoute>
       } />
     </>
