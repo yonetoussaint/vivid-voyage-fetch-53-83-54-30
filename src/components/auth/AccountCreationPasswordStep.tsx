7a35983e9d445215ff45e97b1612bbaf25fa1245
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-11-11 22:35:33 -0500

    Update AccountCreationPasswordStep.tsx

--- src/components/auth/AccountCreationPasswordStep.tsx
+++ src/components/auth/AccountCreationPasswordStep.tsx
@@ -104,7 +104,7 @@ const AccountCreationPasswordStep: React.FC<AccountCreationPasswordStepProps> = 
   const loading = isLoading || parentLoading;
 
   return (
-    <div className="flex flex-col px-4 pb-16">
+    <div className="flex flex-col px-4 pb-24">
       {/* Header - hide in compact mode */}
       {!isCompact && (
         <div className="pt-2 pb-3 flex items-center justify-between">
