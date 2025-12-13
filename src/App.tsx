3aecec3c2d716a737069e6487cfa6014cfbb74fa
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-12-13 17:05:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Providers } from "./components/Providers";
 import { Toasters } from "./components/Toasters";
+import Index from "./pages/Index"; // Import the Index page
 import ProfilePage from "./pages/ProfilePage";
 import CouponsPage from "./pages/CouponsPage";
 import "./App.css";
@@ -12,7 +13,8 @@ function App() {
       <Providers>
         <div className="App min-h-screen h-full bg-background text-foreground flex flex-col">
           <Routes>
-            <Route path="/" element={<ProfilePage />} />
+            <Route path="/" element={<Index />} /> {/* Index as homepage */}
+            <Route path="/profile" element={<ProfilePage />} />
             <Route path="/coupons" element={<CouponsPage />} />
             {/* Add other routes here as needed */}
           </Routes>
