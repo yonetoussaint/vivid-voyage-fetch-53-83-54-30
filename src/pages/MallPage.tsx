79e671570b3985a3d395972675cd201edf93509e
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-12-25 07:51:09 -0500

    Update MallPage.tsx

--- src/pages/MallPage.tsx
+++ src/pages/MallPage.tsx
@@ -3,9 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import FlashDeals from "@/components/home/FlashDeals";
 import FavouriteChannels from "@/components/FavouriteChannels";
 import InfiniteContentGrid from "@/components/InfiniteContentGrid";
-import FilterTabs from "@/components/FilterTabs";
 import HeroBanner from "@/components/home/HeroBanner";
-import Footer from "@/components/Footer";
 import { useMallFilters, useMallData } from "@/hooks/mall.hooks";
 
 interface MallPageProps {
@@ -14,52 +12,38 @@ interface MallPageProps {
 
 const MallPage: React.FC<MallPageProps> = ({ category = 'mall' }) => {
   const [activeCategory, setActiveCategory] = useState(category);
-  const [activeSubcategory, setActiveSubcategory] = useState<string>('all');
 
-  // Use custom hooks
-  const {
-    filters,
-    setFilters,
-    mallTabs,
-    activeFilters,
-    handleTabChange,
-    handleRemoveFilter,
-    handleClearAll,
-  } = useMallFilters();
-
+  // We still use the mall hooks for data, but ignore the filter-related parts
   const {
     mallChannels,
-    getSubcategoryFilters,
   } = useMallData();
 
-  // Create safe filters for InfiniteContentGrid
+  // Create empty filters since we're not showing filter tabs
   const safeFilters = useMemo(() => {
     return {
-      priceRange: filters.priceRange,
-      rating: filters.rating,
-      freeShipping: filters.freeShipping,
-      onSale: filters.onSale,
-      freeReturns: filters.freeReturns,
-      newArrivals: filters.newArrivals,
-      shippedFrom: filters.shippedFrom,
-      sortBy: filters.sortBy,
-      // Add other filters as needed
-      brand: filters.brand,
-      category: filters.category,
-      color: filters.color,
-      sellerRating: filters.sellerRating,
-      fastDispatch: filters.fastDispatch,
-      discount: filters.discount,
-      verifiedSeller: filters.verifiedSeller,
-      mallExclusive: filters.mallExclusive,
+      priceRange: null,
+      rating: null,
+      freeShipping: false,
+      onSale: false,
+      freeReturns: false,
+      newArrivals: false,
+      shippedFrom: null,
+      sortBy: 'popular',
+      brand: null,
+      category: null,
+      color: null,
+      sellerRating: null,
+      fastDispatch: false,
+      discount: false,
+      verifiedSeller: false,
+      mallExclusive: false,
     };
-  }, [filters]);
+  }, []);
 
-  // Remove the handleSubcategorySelect function since channels are not selectable
-  // Optional: If you want to keep some functionality but not selection, you can add a different handler
+  // Optional: If you want channels to have some visual feedback
   const handleChannelClick = () => {
-    // Optional: Add any other behavior when channels are clicked
-    console.log('Channel clicked (non-selectable)');
+    // Do nothing or add minimal feedback
+    console.log('Channel clicked');
   };
 
   useEffect(() => {
@@ -79,8 +63,7 @@ const MallPage: React.FC<MallPageProps> = ({ category = 'mall' }) => {
     <div key="favourite-channels-wrapper" className="">
       <FavouriteChannels 
         channels={mallChannels}
-        // Don't pass activeChannel prop to make them non-selectable
-        onChannelSelect={handleChannelClick} // Optional: Still pass a handler if you want click feedback
+        onChannelSelect={handleChannelClick}
       />
     </div>,
 
@@ -96,19 +79,9 @@ const MallPage: React.FC<MallPageProps> = ({ category = 'mall' }) => {
 
     <div key="separator-2" className="w-full bg-gray-100 h-1 mb-2"></div>,
 
-    <div key="filter-tabs-wrapper" className="pt-2">
-      <FilterTabs
-        tabs={mallTabs}
-        activeFilters={activeFilters}
-        onTabChange={handleTabChange}
-        onRemoveFilter={handleRemoveFilter}
-        onClearAll={handleClearAll}
-      />
-    </div>,
-
     <div key="infinite-grid-wrapper" className="pt-2">
       <InfiniteContentGrid 
-        category={activeCategory} // Just use the main category since subchannels aren't selectable
+        category={activeCategory}
         filters={safeFilters} 
       />
     </div>,
