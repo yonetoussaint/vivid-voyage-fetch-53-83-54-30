28a9c9a1b92e3084de6f2f2d2c38ccabe037ef0f
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-12-26 06:06:43 -0500

    Update AuthRoutes.tsx

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,52 +1,116 @@
-// routes/AuthRoutes.tsx
+// routes/UserRoutes.tsx
 import React from "react";
 import { Route } from "react-router-dom";
 import CachedRoute from "../components/CachedRoute";
-import SimpleAuthPage from "../pages/SimpleAuthPage";
-import AuthPage from "../pages/AuthPage";
-import ForYou from "../pages/ForYou";
-import AuthCallback from "../pages/AuthCallback";
-import AuthErrorPage from "../pages/AuthErrorPage";
-import PrivacyPolicy from "../pages/PrivacyPolicy"; // Add this import
-import TermsOfService from "../pages/TermsOfService"; // Add this import
+import Wallet from "../pages/Wallet";
+import Messages from '../pages/Messages'; // Remove {} - it's a default export
+import { ChatPage } from '../pages/Messages'; // Keep {} for named export
+// Remove: import ConversationDetail from "../pages/ConversationDetail";
+import ProfilePage from "../pages/ProfilePage";
+import MoreMenu from "../pages/MoreMenu";
+import Explore from '../pages/Explore';
+import Wishlist from '../pages/Wishlist';
+import Notifications from '../pages/Notifications';
+import Addresses from '../pages/Addresses";
+import Help from '../pages/Help';
+import MyStations from "../pages/MyStations";
+import ProductsPage from "../pages/ProductsPage";
+import CartPage from '../pages/CartPage';
+import MenuPage from '../pages/MenuPage';
 
-export function AuthRoutes() {
+export function UserRoutes() {
   return (
     <>
-      <Route path="auth" element={
-        <CachedRoute shouldCache={false}>
-          <SimpleAuthPage />
-        </CachedRoute>
-      } />
-      <Route path="signin" element={
-        <CachedRoute shouldCache={false}>
-          <AuthPage />
-        </CachedRoute>
-      } />
-      <Route path="signup" element={
-        <CachedRoute shouldCache={false}>
-          <SimpleAuthPage />
-        </CachedRoute>
-      } />
-      <Route path="auth/callback" element={
+      <Route path="wallet" element={
         <CachedRoute>
-          <AuthCallback />
+          <Wallet />
         </CachedRoute>
       } />
-      <Route path="auth/error" element={
+
+      {/* Messages list */}
+      <Route path="messages" element={
         <CachedRoute>
-          <AuthErrorPage />
+          <Messages />
         </CachedRoute>
       } />
-      {/* Add these new routes for OAuth requirements */}
-      <Route path="privacy" element={
+
+      {/* Chat detail page - Use ChatPage from Messages.tsx */}
+      <Route path="messages/:conversationId" element={
         <CachedRoute>
-          <PrivacyPolicy />
+          <ChatPage />
         </CachedRoute>
       } />
-      <Route path="terms" element={
+
+      {/* Remove the duplicate route below */}
+      {/* <Route path="/messages/:conversationId" element={<ChatPage />} /> */}
+
+      <Route path="profile/*" element={
         <CachedRoute>
-          <TermsOfService />
+          <ProfilePage />
+        </CachedRoute>
+      } />
+
+      <Route path="more" element={
+        <CachedRoute>
+          <MoreMenu />
+        </CachedRoute>
+      } />
+      <Route path="more-menu" element={
+        <CachedRoute>
+          <MoreMenu />
+        </CachedRoute>
+      } />
+      <Route path="explore" element={
+        <CachedRoute>
+          <Explore />
+        </CachedRoute>
+      } />
+      <Route path="wishlist" element={
+        <CachedRoute>
+          <Wishlist />
+        </CachedRoute>
+      } />
+      <Route path="notifications" element={
+        <CachedRoute>
+          <Notifications />
+        </CachedRoute>
+      } />
+      <Route path="addresses" element={
+        <CachedRoute>
+          <Addresses />
+        </CachedRoute>
+      } />
+      <Route path="help" element={
+        <CachedRoute>
+          <Help />
+        </CachedRoute>
+      } />
+      
+      {/* FIXED: Changed from "/my-stations" to "my-stations" */}
+      <Route path="my-stations" element={
+        <CachedRoute>
+          <MyStations />
+        </CachedRoute>
+      } />
+      
+      {/* FIXED: Changed from "/products" to "products" */}
+      <Route path="products" element={
+        <CachedRoute>
+          <ProductsPage />
+        </CachedRoute>
+      } />
+      
+      {/* FIXED: Changed from "/cart" to "cart" */}
+      <Route path="cart" element={
+        <CachedRoute>
+          <CartPage />
+        </CachedRoute>
+      } />
+      
+      {/* FIXED: Changed from "/menu" to "menu" */}
+      <Route path="menu" element={
+        <CachedRoute>
+          <MenuPage />
         </CachedRoute>
       } />
     </>
