ede34528c5706b28367e8a82ab860138e7e6bb6d
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-12-26 06:48:11 -0500

    Update AppRoutes.tsx

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,7 +1,24 @@
-// components/AppRoutes.tsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+import { CategoryRoutes } from "../routes/CategoryRoutes";
+import { ContentRoutes } from "../routes/ContentRoutes";
+import { AuthRoutes } from "../routes/AuthRoutes";
+import { AdminSellerRoutes } from "../routes/AdminSellerRoutes";
+import { ProductEditRoutes } from "../routes/ProductEditRoutes";
+import { PaymentRoutes } from "../routes/PaymentRoutes";
+import { MiscRoutes } from "../routes/MiscRoutes";
+import AdminPage from "@/pages/AdminPage";
+import MallPage from "@/pages/MallPage";
+import CachedRoute from "./CachedRoute";
+import ConditionalHomepage from "./layout/ConditionalHomepage";
+import Index from "@/pages/Index";
+import ProductDetail from "@/pages/ProductDetail";
+import SingleProductDetail from "@/pages/SingleProductDetail";
+import ProductDescriptionPage from "@/pages/ProductDescriptionPage";
+import ProductCommentsPage from "@/pages/ProductCommentsPage";
+import ProductQAPage from "@/pages/ProductQAPage";
+import AskQuestionPage from "@/pages/AskQuestionPage";
 import Wallet from "@/pages/Wallet";
 import Messages from '@/pages/Messages';
 import { ChatPage } from '@/pages/Messages';
@@ -9,48 +26,192 @@ import ProfilePage from "@/pages/ProfilePage";
 import MoreMenu from "@/pages/MoreMenu";
 import Explore from '@/pages/Explore';
 import Wishlist from '@/pages/Wishlist';
-import Notifications from "@/pages/Notifications";
-import Addresses from "@/pages/Addresses";
-import Help from "@/pages/Help";
+import Notifications from '@/pages/Notifications';
+import Addresses from '@/pages/Addresses";
+import Help from '@/pages/Help';
 import MyStations from "@/pages/MyStations";
 import ProductsPage from "@/pages/ProductsPage";
 import CartPage from '@/pages/CartPage';
 import MenuPage from '@/pages/MenuPage';
-import AdminPage from "@/pages/AdminPage";
-import MallPage from "@/pages/MallPage";
-// Import other pages as needed
 
 export function AppRoutes() {
   return (
     <Routes>
       {/* All routes inside MainLayout */}
       <Route path="/" element={<MainLayout />}>
-        {/* User Routes */}
-        <Route path="/wallet" element={<Wallet />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/messages/:conversationId" element={<ChatPage />} />
-        <Route path="/profile/*" element={<ProfilePage />} />
-        <Route path="/more" element={<MoreMenu />} />
-        <Route path="/more-menu" element={<MoreMenu />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/addresses" element={<Addresses />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/my-stations" element={<MyStations />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-
-        {/* Mall Route */}
-        <Route path="/mall" element={<MallPage />} />
-
-        {/* Admin Route */}
-        <Route path="/admin" element={<AdminPage />} />
-
-        {/* Add other routes from your route functions here */}
-        {/* Example: Homepage, Product, Category routes, etc. */}
+        {/* Homepage routes */}
+        <Route index element={
+          <CachedRoute>
+            <ConditionalHomepage />
+          </CachedRoute>
+        } />
+        <Route path="for-you" element={
+          <CachedRoute>
+            <ConditionalHomepage />
+          </CachedRoute>
+        } />
+        <Route path="index" element={
+          <CachedRoute>
+            <Index />
+          </CachedRoute>
+        } />
         
+        {/* Product routes */}
+        <Route path="product/:id/:tab" element={
+          <CachedRoute>
+            <ProductDetail />
+          </CachedRoute>
+        } />
+
+        <Route path="product/:id" element={
+          <Navigate to="overview" replace />
+        } />
+
+        <Route path="product/:id/description" element={
+          <CachedRoute>
+            <ProductDescriptionPage />
+          </CachedRoute>
+        } />
+
+        <Route path="product/:id/comments" element={
+          <CachedRoute>
+            <ProductCommentsPage />
+          </CachedRoute>
+        } />
+
+        <Route path="product/:id/qa" element={
+          <CachedRoute>
+            <ProductQAPage />
+          </CachedRoute>
+        } />
+
+        <Route path="product/:id/ask-question" element={
+          <CachedRoute>
+            <AskQuestionPage />
+          </CachedRoute>
+        } />
+
+        <Route path="single-product/:id" element={
+          <CachedRoute>
+            <SingleProductDetail />
+          </CachedRoute>
+        } />
+
+        <Route path="single-product/:id/comments" element={
+          <CachedRoute>
+            <ProductCommentsPage />
+          </CachedRoute>
+        } />
+
+        <Route path="single-product/:id/ask-question" element={
+          <CachedRoute>
+            <AskQuestionPage />
+          </CachedRoute>
+        } />
+
+        {/* User routes moved from UserRoutes.tsx */}
+        <Route path="wallet" element={
+          <CachedRoute>
+            <Wallet />
+          </CachedRoute>
+        } />
+
+        {/* Messages list */}
+        <Route path="messages" element={
+          <CachedRoute>
+            <Messages />
+          </CachedRoute>
+        } />
+
+        {/* Chat detail page */}
+        <Route path="messages/:conversationId" element={
+          <CachedRoute>
+            <ChatPage />
+          </CachedRoute>
+        } />
+
+        <Route path="profile/*" element={
+          <CachedRoute>
+            <ProfilePage />
+          </CachedRoute>
+        } />
+
+        <Route path="more" element={
+          <CachedRoute>
+            <MoreMenu />
+          </CachedRoute>
+        } />
+        <Route path="more-menu" element={
+          <CachedRoute>
+            <MoreMenu />
+          </CachedRoute>
+        } />
+        <Route path="explore" element={
+          <CachedRoute>
+            <Explore />
+          </CachedRoute>
+        } />
+        <Route path="wishlist" element={
+          <CachedRoute>
+            <Wishlist />
+          </CachedRoute>
+        } />
+        <Route path="notifications" element={
+          <CachedRoute>
+            <Notifications />
+          </CachedRoute>
+        } />
+        <Route path="addresses" element={
+          <CachedRoute>
+            <Addresses />
+          </CachedRoute>
+        } />
+        <Route path="help" element={
+          <CachedRoute>
+            <Help />
+          </CachedRoute>
+        } />
+
+        <Route path="my-stations" element={
+          <CachedRoute>
+            <MyStations />
+          </CachedRoute>
+        } />
+
+        <Route path="products" element={
+          <CachedRoute>
+            <ProductsPage />
+          </CachedRoute>
+        } />
+
+        <Route path="cart" element={
+          <CachedRoute>
+            <CartPage />
+          </CachedRoute>
+        } />
+
+        <Route path="menu" element={
+          <CachedRoute>
+            <MenuPage />
+          </CachedRoute>
+        } />
+
+        {/* Call the remaining route functions */}
+        {CategoryRoutes()}
+        {ContentRoutes()}
+        {AuthRoutes()}
+        {AdminSellerRoutes()}
+        {ProductEditRoutes()}
+        {PaymentRoutes()}
+        {MiscRoutes()}
+
+        {/* Add Mall Route directly */}
+        <Route path="mall" element={<MallPage />} />
+
+        {/* Add Admin Route */}
+        <Route path="admin" element={<AdminPage />} />
+        
+        {/* CommunesPage is handled as a modal in MainLayout, not as a route */}
       </Route>
     </Routes>
   );
