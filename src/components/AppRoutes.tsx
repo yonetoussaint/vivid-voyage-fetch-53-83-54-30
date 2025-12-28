b396a8efe504b2d789ea1bd6d6237c02af5a4ca7
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-12-28 12:12:50 -0500

    Update AppRoutes.tsx

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,36 +4,14 @@ import MainLayout from "./layout/MainLayout";
 import { CategoryRoutes } from "../routes/CategoryRoutes";
 import { ContentRoutes } from "../routes/ContentRoutes";
 import { AuthRoutes } from "../routes/AuthRoutes";
-import { AdminSellerRoutes } from "../routes/AdminSellerRoutes";
-import { ProductEditRoutes } from "../routes/ProductEditRoutes";
-import { PaymentRoutes } from "../routes/PaymentRoutes";
 import { MiscRoutes } from "../routes/MiscRoutes";
-import AdminPage from "@/pages/AdminPage";
 import MallPage from "@/pages/MallPage";
-import CachedRoute from "./CachedRoute";
-import ConditionalHomepage from "./layout/ConditionalHomepage";
-import Index from "@/pages/Index";
-import ProductDetail from "@/pages/ProductDetail";
-import SingleProductDetail from "@/pages/SingleProductDetail";
-import ProductDescriptionPage from "@/pages/ProductDescriptionPage";
-import ProductCommentsPage from "@/pages/ProductCommentsPage";
-import ProductQAPage from "@/pages/ProductQAPage";
-import AskQuestionPage from "@/pages/AskQuestionPage";
 import Wallet from "@/pages/Wallet";
 import Messages from "@/pages/Messages";
 import { ChatPage } from "@/pages/Messages";
 import ProfilePage from "@/pages/ProfilePage";
-import MoreMenu from "@/pages/MoreMenu";
-import Explore from "@/pages/Explore";
-import Wishlist from "@/pages/Wishlist";
-import Notifications from "@/pages/Notifications";
-import Addresses from "@/pages/Addresses";
-import Help from "@/pages/Help";
-import MyStations from "@/pages/MyStations";
-import ProductsPage from "@/pages/ProductsPage";
-import CartPage from "@/pages/CartPage";
-import MenuPage from "@/pages/MenuPage";
-import CommunesPage from "@/pages/CommunesPage";
+import ProductDetail from "@/pages/ProductDetail";
+import ProductCommentsPage from "@/pages/ProductCommentsPage";
 
 export function AppRoutes() {
   return (
@@ -42,74 +20,16 @@ export function AppRoutes() {
       <Route
         path="product/:id/:tab"
         element={
-          <CachedRoute>
-            <ProductDetail />
-          </CachedRoute>
+          <ProductDetail />
         }
       />
-      
-      <Route path="product/:id" element={<Navigate to="overview" replace />} />
 
-      <Route
-        path="product/:id/description"
-        element={
-          <CachedRoute>
-            <ProductDescriptionPage />
-          </CachedRoute>
-        }
-      />
+      <Route path="product/:id" element={<Navigate to="overview" replace />} />
 
       <Route
         path="product/:id/comments"
         element={
-          <CachedRoute>
-            <ProductCommentsPage />
-          </CachedRoute>
-        }
-      />
-
-      <Route
-        path="product/:id/qa"
-        element={
-          <CachedRoute>
-            <ProductQAPage />
-          </CachedRoute>
-        }
-      />
-
-      <Route
-        path="product/:id/ask-question"
-        element={
-          <CachedRoute>
-            <AskQuestionPage />
-          </CachedRoute>
-        }
-      />
-
-      <Route
-        path="single-product/:id"
-        element={
-          <CachedRoute>
-            <SingleProductDetail />
-          </CachedRoute>
-        }
-      />
-
-      <Route
-        path="single-product/:id/comments"
-        element={
-          <CachedRoute>
-            <ProductCommentsPage />
-          </CachedRoute>
-        }
-      />
-
-      <Route
-        path="single-product/:id/ask-question"
-        element={
-          <CachedRoute>
-            <AskQuestionPage />
-          </CachedRoute>
+          <ProductCommentsPage />
         }
       />
 
@@ -118,177 +38,50 @@ export function AppRoutes() {
         <Route
           index
           element={
-            <CachedRoute>
-              <ConditionalHomepage />
-            </CachedRoute>
+            <MallPage />
           }
         />
         <Route
           path="for-you"
           element={
-            <CachedRoute>
-              <ConditionalHomepage />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="index"
-          element={
-            <CachedRoute>
-              <Index />
-            </CachedRoute>
+            <MallPage />
           }
         />
 
         <Route
           path="wallet"
           element={
-            <CachedRoute>
-              <Wallet />
-            </CachedRoute>
+            <Wallet />
           }
         />
 
         <Route
           path="messages"
           element={
-            <CachedRoute>
-              <Messages />
-            </CachedRoute>
+            <Messages />
           }
         />
 
         <Route
           path="messages/:conversationId"
           element={
-            <CachedRoute>
-              <ChatPage />
-            </CachedRoute>
+            <ChatPage />
           }
         />
 
         <Route
           path="profile/*"
           element={
-            <CachedRoute>
-              <ProfilePage />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="more"
-          element={
-            <CachedRoute>
-              <MoreMenu />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="more-menu"
-          element={
-            <CachedRoute>
-              <MoreMenu />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="explore"
-          element={
-            <CachedRoute>
-              <Explore />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="wishlist"
-          element={
-            <CachedRoute>
-              <Wishlist />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="notifications"
-          element={
-            <CachedRoute>
-              <Notifications />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="addresses"
-          element={
-            <CachedRoute>
-              <Addresses />
-            </CachedRoute>
-          }
-        />
-        <Route
-          path="help"
-          element={
-            <CachedRoute>
-              <Help />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="my-stations"
-          element={
-            <CachedRoute>
-              <MyStations />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="products"
-          element={
-            <CachedRoute>
-              <ProductsPage />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="cart"
-          element={
-            <CachedRoute>
-              <CartPage />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="menu"
-          element={
-            <CachedRoute>
-              <MenuPage />
-            </CachedRoute>
-          }
-        />
-
-        <Route
-          path="communes"
-          element={
-            <CachedRoute>
-              <CommunesPage />
-            </CachedRoute>
+            <ProfilePage />
           }
         />
 
         {CategoryRoutes()}
         {ContentRoutes()}
         {AuthRoutes()}
-        {AdminSellerRoutes()}
-        {ProductEditRoutes()}
-        {PaymentRoutes()}
         {MiscRoutes()}
 
         <Route path="mall" element={<MallPage />} />
-
-        <Route path="admin" element={<AdminPage />} />
       </Route>
     </Routes>
   );
