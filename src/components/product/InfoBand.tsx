bcdaf73de8d9a7fee2b39cfd31fd7ad8066c15ca
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-01-05 13:32:28 -0500

    Update InfoBand.tsx

--- src/components/product/InfoBand.tsx
+++ src/components/product/InfoBand.tsx
@@ -199,7 +199,7 @@ const InfoBand = () => {
       <div 
         className={`${currentBand.color} w-full transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
       >
-        <div className="flex items-center justify-center py-1 px-4 text-white">
+        <div className="flex items-center justify-center px-4 text-white">
           <IconComponent className="mr-2 h-5 w-5" />
           <p className="font-medium text-sm">{currentBand.text}</p>
         </div>
