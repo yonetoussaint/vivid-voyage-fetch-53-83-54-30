438ff0b544dbb7520808bd05f70fce5800076125
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-01-16 18:33:46 -0500

    Update DepotsManager.tsx

--- src/components/easy/DepotsManager.tsx
+++ src/components/easy/DepotsManager.tsx
@@ -24,11 +24,9 @@ const DepotsManager = ({ shift, vendeurs, totauxVendeurs, tousDepots, mettreAJou
     { value: '1', label: '1' },
     { value: '5', label: '5' },
     { value: '10', label: '10' },
-    { value: '20', label: '20' },
     { value: '25', label: '25' },
     { value: '50', label: '50' },
     { value: '100', label: '100' },
-    { value: '200', label: '200' },
     { value: '250', label: '250' },
     { value: '500', label: '500' },
     { value: '1000', label: '1,000' }
