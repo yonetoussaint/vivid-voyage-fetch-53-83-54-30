c6fa2eff501ded10f03e7879e36f954b684f6e8f
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-01-31 07:24:50 -0500

    Update MainLayout.tsx

--- src/components/easy/MainLayout.tsx
+++ src/components/easy/MainLayout.tsx
@@ -13,9 +13,9 @@ const MainLayout = ({
   onShiftChange 
 }) => {
   return (
-    <>
-      {/* Header outside of any flex container - fixed at top */}
-      <div className="sticky top-0 z-50">
+    <div className="h-screen flex flex-col">
+      {/* Fixed Header Container - This is what makes it sticky */}
+      <div className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-200">
         <Header
           date={date}
           shift={shift}
@@ -26,23 +26,26 @@ const MainLayout = ({
         />
       </div>
 
+      {/* Spacer to prevent content from hiding behind fixed header */}
+      <div className="h-16 flex-shrink-0"></div>
+
       {/* Main Content Area */}
-      <div className="flex min-h-screen bg-gray-50 pt-16"> {/* Add padding-top equal to header height */}
-        {/* Side Panel for desktop - always visible */}
+      <div className="flex flex-1 min-h-0">
+        {/* Side Panel for desktop */}
         <div className="hidden lg:block flex-shrink-0">
           <SidePanel isOpen={true} onClose={() => {}} isMobile={false}>
             <VerticalTabs activeTab={activeTab} onTabChange={() => {}} isMobile={false} />
           </SidePanel>
         </div>
 
-        {/* Main Content - scrolls independently */}
+        {/* Main Content - This will scroll */}
         <main className="flex-1 overflow-auto">
           <div className="p-2 sm:p-4">
-            {children}         
+            {children}
           </div>
         </main>
       </div>
-    </>
+    </div>
   );
 };
 
