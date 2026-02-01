801feac600193ae6d20fd5942a2c4e1f3f168260
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-01 10:55:18 -0500

    Create NoVendorsMessage.tsx

---
+++ src/components/easy/NoVendorsMessage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoVendorsMessage = () => {
+  return (
+    <div className="text-center py-6 text-white text-opacity-70">
+      Aucun vendeur ajouté
+    </div>
+  );
+};
+
+export default NoVendorsMessage;
