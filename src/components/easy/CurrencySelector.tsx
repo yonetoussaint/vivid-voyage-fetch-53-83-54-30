5f77f57ddc48bbbe380594483d540e7a621b3fc2
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-02 22:59:52 -0500

    Update CurrencySelector.tsx

--- src/components/easy/CurrencySelector.tsx
+++ src/components/easy/CurrencySelector.tsx
@@ -6,7 +6,7 @@ const CurrencySelector = ({ currency, onCurrencyChange }) => (
     {/* HTG */}
     <div
       onClick={() => onCurrencyChange('HTG')}
-      className={`flex-1 flex items-center justify-center gap-2 cursor-pointer rounded-full transition-all duration-200 ${
+      className={`flex-1 flex items-center justify-center py-1 gap-2 cursor-pointer rounded-full transition-all duration-200 ${
         currency === 'HTG' ? 'bg-blue-500 text-white' : 'text-gray-700'
       }`}
     >
@@ -17,7 +17,7 @@ const CurrencySelector = ({ currency, onCurrencyChange }) => (
     {/* USD */}
     <div
       onClick={() => onCurrencyChange('USD')}
-      className={`flex-1 flex items-center justify-center gap-2 cursor-pointer rounded-full transition-all duration-200 ${
+      className={`flex-1 flex items-center justify-center py-1 gap-2 cursor-pointer rounded-full transition-all duration-200 ${
         currency === 'USD' ? 'bg-green-500 text-white' : 'text-gray-700'
       }`}
     >
