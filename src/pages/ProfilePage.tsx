1d6f9f82cb5c7b8290a450bfa5ad47984404d80a
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-09 06:55:16 -0500

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -279,7 +279,7 @@ export default function XProfile() {
 
       {/* Tabs */}
       <div className="border-b border-gray-200">
-        <div className="flex px-4">
+        <div className="flex px-2">
           {['Products', 'Reviews', 'Media', 'Likes'].map((tab) => (
             <button
               key={tab}
