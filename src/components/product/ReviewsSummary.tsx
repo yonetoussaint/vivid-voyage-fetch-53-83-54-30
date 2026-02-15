075dd02c0f86acd3a2e27e8516218aafb976ae7e
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-15 08:41:05 -0500

    Update ReviewsSummary.tsx

--- src/components/product/ReviewsSummary.tsx
+++ src/components/product/ReviewsSummary.tsx
@@ -36,15 +36,15 @@ const CalendarIcon = () => (
 );
 
 const VerifiedBadgeIcon = () => (
-  <svg viewBox="0 0 24 24" fill="none" className="w-full h-full" xmlns="http://www.w3.org/2000/svg">
-    <path 
-      d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z" 
-      fill="#3B82F6" // Blue fill
-    />
-    <path 
-      d="M10 14.17L16.59 7.58C17.05 7.12 17.77 7.12 18.23 7.58C18.69 8.04 18.69 8.76 18.23 9.22L11.13 16.32C10.85 16.6 10.48 16.78 10.07 16.78C9.66 16.78 9.29 16.6 9.01 16.32L5.91 13.22C5.45 12.76 5.45 12.04 5.91 11.58C6.37 11.12 7.09 11.12 7.55 11.58L10 14.17Z" 
-      fill="white" // White checkmark
-    />
+  <svg 
+    width="100%" 
+    height="100%" 
+    viewBox="0 0 128 128" 
+    preserveAspectRatio="xMidYMid meet"
+    className="w-full h-full"
+  >
+    <path d="M53 9c2.309 1.383 2.309 1.383 4.625 3.063 3.385 2.491 3.385 2.491 7.5 3.124 2.574-1.063 4.098-2.063 6.25-3.75 2.613-1.983 4.454-2.87 7.75-3.062C83 9 83 9 85.75 10.813 88.855 15.21 90.398 19.89 92 25l2.633.113c11.734.67 11.734.67 16.367 3.887 2.165 4.331 1.395 9.793-.062 14.25L110 46c-.319 2.576-.319 2.576 1 5 1.915 1.646 3.89 3.073 5.992 4.473 3.135 2.385 4.795 4.461 5.57 8.34-.912 5.173-4.204 8.376-8.296 11.519-1.391.937-2.825 1.81-4.266 2.668l.438 1.617C111.87 85.366 112.883 91.082 112 97c-2.191 3.098-3.41 3.804-7 5-2.226.225-4.456.408-6.687.563l-3.575.253L92 103l-.953 2.738-1.297 3.575-1.266 3.55C86.967 116.07 86.076 117.331 83 119c-6.747 1.03-9.514-.801-14.898-4.715-2.154-1.552-2.154-1.552-5.352-1.41-3.109 1.272-5.219 2.936-7.75 5.125-3.638 1.213-6.177 1.395-10 1-5.617-3.788-7.06-9.813-9-16l-2.633-.113c-11.734-.67-11.734-.67-16.367-3.887-2.165-4.331-1.395-9.793.063-14.25L18 82c.319-2.576.319-2.576-1-5-1.915-1.646-3.89-3.073-5.992-4.473-3.135-2.385-4.795-4.461-5.57-8.34.912-5.173 4.204-8.376 8.296-11.519C15.125 51.731 16.56 50.858 18 50l-.437-1.617C16.13 42.634 15.117 36.918 16 31c2.191-3.098 3.41-3.804 7-5 2.226-.225 4.456-.408 6.688-.562l3.574-.254L36 25l.953-2.738 1.297-3.575 1.266-3.55C42.313 9.225 46.63 7.539 53 9" fill="#49ADF4"/>
+    <path d="M80.969 55.168 83 56l1 5a5562 5562 0 0 1-8.625 6.75l-2.45 1.922A673 673 0 0 1 66 75l-1.784 1.38c-3.675 2.688-3.675 2.688-6.192 2.372-3.443-1.28-5.782-3.957-8.336-6.502l-1.706-1.594C46.387 69.078 46.387 69.078 44 66c.145-2.89.145-2.89 1-5 2.23-.746 2.23-.746 5-1 2.238 1.652 2.238 1.652 4.313 3.938l2.113 2.277L58 68c5.678-1.802 9.9-5.595 14.473-9.275 4.892-3.76 4.892-3.76 8.496-3.557" fill="#F9FCFE"/>
   </svg>
 );
 
@@ -116,7 +116,7 @@ const ReviewsSummary: React.FC<ReviewsSummaryProps> = ({
     label: 'Sort by',
     type: 'dropdown',
     value: 'mostRelevant',
-    icon: <ArrowUpDownIcon />, // Custom SVG
+    icon: <ArrowUpDownIcon />,
     options: [
       { label: 'Most Relevant', value: 'mostRelevant' },
       { label: 'Most Recent', value: 'mostRecent' },
@@ -129,7 +129,7 @@ const ReviewsSummary: React.FC<ReviewsSummaryProps> = ({
     label: 'Rating',
     type: 'dropdown',
     value: null,
-    icon: <StarIcon />, // Custom SVG with amber fill
+    icon: <StarIcon />,
     options: [
       { label: '5 Stars', value: 5 },
       { label: '4 Stars', value: 4 },
@@ -143,7 +143,7 @@ const ReviewsSummary: React.FC<ReviewsSummaryProps> = ({
     label: 'Time Period',
     type: 'dropdown',
     value: null,
-    icon: <CalendarIcon />, // Custom SVG with purple fill
+    icon: <CalendarIcon />,
     options: [
       { label: 'All Time', value: 'all' },
       { label: 'Last Week', value: 'week' },
@@ -157,7 +157,7 @@ const ReviewsSummary: React.FC<ReviewsSummaryProps> = ({
     label: 'Verified Purchase',
     type: 'checkbox',
     value: false,
-    icon: <VerifiedBadgeIcon /> // Custom SVG with blue fill and white checkmark
+    icon: <VerifiedBadgeIcon />
   }
 ]);
 
