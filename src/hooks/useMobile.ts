1311956d6362e0ef9cd5ec9dd48da69c5b38b964
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-24 07:13:24 -0500

    Create useMobile.ts

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+export function useIsMobile() {
+  const [mobile, setMobile] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    const fn = () => setMobile(window.innerWidth < 768);
+    window.addEventListener("resize", fn);
+    return () => window.removeEventListener("resize", fn);
+  }, []);
+  return mobile;
+}
