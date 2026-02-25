4cd4d5d77aab73572f21c47ed0469e0aeb5d4234
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-24 21:30:34 -0500

    Update eventUtils.ts

--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -1,5 +1,5 @@
-import { HABITS } from '../data/habitsData';
-import { formatDateKey } from './dateHelpers';
+import { HABITS } from '@/data/habitsData';
+import { formatDateKey } from '@/hooks/dateHelpers';
 
 // Dynamic events store
 export const dynamicEvents = {};
