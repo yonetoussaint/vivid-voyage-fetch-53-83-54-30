b79feb3a4bf8c8925686a6d1739543d371ae1f96
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-02-26 16:08:28 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.5265be0a5a764d54901bfbcb9a1e8249',
-  appName: 'vivid-verse-voyage-05u-51-95-02-25-43',
+  appName: 'TruckApp',
   webDir: 'dist',
-  server: {
-    url: 'https://5265be0a-5a76-4d54-901b-fbcb9a1e8249.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
       launchShowDuration: 3000,
@@ -21,8 +18,6 @@ const config: CapacitorConfig = {
       spinnerColor: "#999999",
       splashFullScreen: true,
       splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
     },
   },
 };
